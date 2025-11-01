3431658e55fa4e65bae42f15453b48183cd095e9
lihi shapira <lihi.shapira@post.runi.ac.il>
2025-11-01 13:48:56 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
    {
 		double currentValue = Double.parseDouble(args[0]);
 		double interestRate = Double.parseDouble(args[1]);
-		double n = Double.parseDouble(args[2]);
-		double futureValue = currentValue * Math.pow(1 + (interestRate / 100), n) ;
-		System.out.println( " After " + (int)n + " years," + " $"+ (int)currentValue + " saved at " +interestRate + "%  will yield $" + (int)futureValue) ;
+		int n = Integer.parseInt(args[2]);
+		double futureValue = currentValue * Math.pow(1 + (interestRate / 100), n);
+		System.out.println( " After " + n + " years, $" + (int)currentValue + " saved at " +interestRate + "%  will yield $" + (int)futureValue) ;
 	}
 	 }
