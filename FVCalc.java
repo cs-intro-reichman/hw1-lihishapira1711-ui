2c47e9b6a3137e26fdf6aaba0a88e53b07142868
lihi shapira <lihi.shapira@post.runi.ac.il>
2025-11-01 12:06:04 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
-// Computes the future value of a saving investment.
+
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args) 
+   {
+		double currentValue = Double.parseDouble(args[0]);
+		double interestRate = Double.parseDouble(args[1]);
+		double n = Double.parseDouble(args[2]);
+		double futureValue = currentValue * Math.pow(1 + (interestRate / 100), n) ;
+		System.out.println( "After " + (int)n + " years," + " $"+ (int)currentValue + " saved at " +interestRate + "%  will yield $" + (int)futureValue) ;
 	}
-}
+	 }
---
+++ PrintTo5.java
@@ -0,0 +1,11 @@
+
+public class PrintTo5 {
+	public static void main(String[] args) {
+        int i = 0;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+       System.out.println("done");
+    }
+ }
